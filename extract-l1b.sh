a6255b3dfc27382d6e235c33da2dd6f7bf95f340
jgte <j.g.deteixeiradaencarnacao@tudelft.nl>
2017-01-19 15:49:42 -0600

    extract specific products explicitly, instead of a predefined subset; if archive is not available, try to download it; relocated the downloaded archives to yearly subdirs

--- extract-l1b.sh
+++ extract-l1b.sh
@@ -3,9 +3,12 @@
 # keeping track of where I am
 DIR_NOW=$(cd $(dirname $BASH_SOURCE); pwd)
 
-if [ $# -lt 1 ]
+if [ $# -lt 2 ]
 then
-  echo "Need at one input argument: <date> in YYYYMMDD"
+  echo "$0 <date> <product>"
+  echo "Need two input arguments."
+  echo "  - <date> in YYYYMMDD"
+  echo "  - <product> name: ACC1B, AHK1B, GNV1B, KBR1B, MAS1B, SCA1B, THR1B, CLK1B, GPS1B, IHK1B, MAG1B, TDP1B, TIM1B, TNK1B, USO1B, VSL1B"
   exit 1
 fi
 
@@ -14,31 +17,34 @@ DATE=$1
 YEAR=${DATE:0:4}
 MONTH=${DATE:4:2}
 DAY=${DATE:6:2}
+PRODUCT=$2
+if [ $# -lt 3 ]
+then
+  SAT='A'
+else
+  SAT=$3
+fi
+[ "$PRODUCT" == "KBR1B" ] && SAT="X"
 
-#filename stuff
+#archive name stuff
 PREFIX=grace_1B
 SUFFIX=.tar.gz
-
 # building package filename and pick the most recent version
-TAR_FILE=$(ls $DIR_NOW/L1B/$YEAR/$MONTH/$DAY/${PREFIX}_$YEAR-$MONTH-${DAY}_*$SUFFIX | tail -n1)
+TAR_FILE=$(ls $DIR_NOW/L1B/$YEAR/${PREFIX}_$YEAR-$MONTH-${DAY}_*$SUFFIX 2> /dev/null | tail -n1)
 
 if [ ! -e "$TAR_FILE" ]
 then
-  echo "Cannot find file '$TAR_FILE'."
+  # need to get it
+  $DIR_NOW/download-l1b.sh $DATE || exit $?
+  # re-building package filename and pick the most recent version
+  TAR_FILE=$(ls $DIR_NOW/L1B/$YEAR/${PREFIX}_$YEAR-$MONTH-${DAY}_*$SUFFIX 2> /dev/null | tail -n1)
+fi
+
+if [ ! -e "$TAR_FILE" ]
+then
+  echo "Cannot find file '$DIR_NOW/L1B/$YEAR/${PREFIX}_$YEAR-$MONTH-${DAY}_*$SUFFIX'."
   exit 3
 fi
 
 # extracting
-DIR_HERE=$(dirname $TAR_FILE)
-mkdir -p $DIR_HERE
-tar -xvmzk -f "$TAR_FILE" -C "$DIR_HERE" \
---exclude=CLK1B* \
---exclude=GPS1B* \
---exclude=IHK1B* \
---exclude=MAG1B* \
---exclude=MAS1B* \
---exclude=TDP1B* \
---exclude=TIM1B* \
---exclude=TNK1B* \
---exclude=USO1B* \
---exclude=VSL1B*
+tar -xvmzk -f "$TAR_FILE" -C "$(dirname $TAR_FILE)" --include=$PRODUCT*_${SAT}_*.dat || exit $?
